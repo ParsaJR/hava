1a51a7ae7c74b4d461d6a791208d1812c8a16fba
ParsaJR <parsajr@proton.me>
2025-04-03 19:20:31 +0330

    tests: using t for the state instead of log

--- pkg/weatherApi_test.go
+++ pkg/weatherApi_test.go
@@ -16,24 +16,24 @@ func TestApi(t *testing.T) {
 	err := godotenv.Load("../.env")
 	apikey := os.Getenv("KEY")
 	if err != nil && len(apikey) == 0 {
-		log.Fatal("API is not exist")
+		t.Fatal("API is not exist")
 	}
 	url := fmt.Sprintf("https://api.weatherapi.com/v1/current.json?key=%s&q=%s&aqi=no", apikey, "Tehran")
 
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to make api request: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Expected %d status code, got %d",http.StatusOK,resp.StatusCode)	
+		t.Errorf("Expected %d status code, got %d", http.StatusOK, resp.StatusCode)
 	}
 	var weatherResponse WeatherResponse
 
 	if err := json.Unmarshal(body, &weatherResponse); err != nil {
-		t.Fatalf("Failed to unmarshal response: %v",err)	
-	} 
+		t.Fatalf("Failed to unmarshal response: %v", err)
+	}
 
 }
